f3f83b8dbbce4238c2770aab8b9ce5cc3c31e462
Apoorva Hemmige Arun <apoorvaharun.cs24@rvce.edu.in>
2025-09-13 20:54:57 +0530

    trial

--- src/database.py
+++ src/database.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # Creating a local SQLite database called 'test'
-DATABASE_URL = "postgresql://postgres:testdatabase@localhost:5432/testdb"
+# DATABASE_URL = "postgresql://postgres:testdatabase@localhost:5432/testdb"
+DATABASE_URL = "postgresql://neondb_owner:npg_jyYMv0OAp3Wb@ep-summer-truth-adbe6t13-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require"
 
 engine = create_engine(DATABASE_URL)
 
